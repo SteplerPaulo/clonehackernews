7126f6fd12127f5f150440ad7e3e9439ea68b0dd
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-04 19:18:08 +0800

    Brand Name Component

---
+++ src/components/Brand.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Brand() {
+    return (
+      <React.Fragment> 
+        <div class="brand">
+          <img  src="/assets/img/hackernewslogo.png" alt="Hacker News Logo"/>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+export default Brand;
+  
