ff95b9377f0d51e73bdb90054c4722d95fa83b9d
SteplerPaulo <paulobiscocho@gmail.com>
2021-05-04 20:07:58 +0800

    Item Card Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Brand from './components/Brand.js'
 import Nav from './components/Nav.js'
+import ItemCard from './components/ItemCard.js'
 
 function App() {
   return (
     <div className="container-fluid">
       <Brand/>
       <Nav/>
+      <ItemCard/>
     </div>
   );
 }
---
+++ src/components/ItemCard.js
@@ -0,0 +1,16 @@
+function ItemCard() {
+    return (
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h6><b class="card-title">Lorem Ipsum is simply dummy text.</b></h6>
+                        <p class="card-text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, …when an unknown printer took a galley of type and scrambled.</p>
+                        <small class="text-muted">1 min ago | 50 comments</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default ItemCard;
