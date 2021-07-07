70d016b7d9a40d0179af52740c2d211ca96d751b
SteplerPaulo <paulobiscocho@gmail.com>
2021-07-07 19:04:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Default from "layouts/Default.js";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <React.StrictMode>
+    <BrowserRouter history={hist}>
+      <Switch>
+        <Route exact path="/" component={Default} />
+      </Switch>
+    </BrowserRouter>,
+  </React.StrictMode>,
+  document.getElementById("root")
+);
